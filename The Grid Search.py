adb1b6e3c09e81e4c1ff5527a40e6a5efe3b50cd
Jaivanth B <beerakajaivanth15@gmail.com>
2024-04-25 19:20:02 +0530

    Update The Grid Search.py

--- The Grid Search.py
+++ The Grid Search.py
@@ -17,7 +17,26 @@ import sys
 
 def gridSearch(G, P):
     # Write your code here
-
+ R=len(G[0]); r=len(P[0])
+ x=".".join(G)
+ s=0
+ if P[0] in x:
+  while P[0] in x[s:]:
+   l=x[s:].index(P[0])
+   il=l
+   f=1
+   for i in range(1,len(P)):
+    print(l,P[i],x[s+l+R:s+l+R+len(P[i])])
+    if x[s+l+R+1:s+l+R+1+len(P[i])]!=P[i]:
+     s=s+il+1
+     f=0
+     break
+    else:
+     l=l+R+1
+   print(s,i)
+   if(f):
+    return "YES"
+ return "NO"
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
