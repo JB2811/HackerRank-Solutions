fb30168e9dd0a2407d1ac5d651956d3d13964230
Jaivanth B <beerakajaivanth15@gmail.com>
2024-04-25 19:19:32 +0530

    Update Sherlock and the Valid String.py

--- Sherlock and the Valid String.py
+++ Sherlock and the Valid String.py
@@ -14,8 +14,27 @@ import sys
 #
 
 def isValid(s):
-    # Write your code here
-
+ # Write your code here
+ d=[]
+ e=set(s)
+ for i in e:
+  d.append(s.count(i))
+ t=d[0]
+ i=0
+ c=1
+ while(i<2 and i<len(d)):
+  if(c<d.count(d[i])):
+   t=d[i]
+   c=d.count(t)
+  i+=1
+ print(d,t,c,sum(d))
+ if(len(set(d))<=2):
+  if(c==len(d) or sum(d)==((t*(c+1))+1) or sum(d)==((t*c)+1)):
+   return "YES"
+  else:
+   return "NO"
+ else:
+  return "NO"
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
